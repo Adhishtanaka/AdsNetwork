bb673a2ea8bf20c96ef0e5f618a3cb0add38d6fc
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-08-15 17:59:56 +0530

    added new routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Profile from './pages/auth/Profile';
 import AddAdPage from './pages/ads/AddAds';
 import SearchAds from './pages/ads/AllAds';
 import SingleAdPage from './pages/ads/SingleAds';
+import UpdateAdPage from './pages/ads/UpdateAds';
 
 function App() {
   return (
@@ -16,13 +17,22 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/ads/add" element={<AddAdPage />} />
+            <Route path="/ads/add" element={
+              <ProtectedRoute>
+              <AddAdPage />
+              </ProtectedRoute>
+              } />
+              <Route path="/ads/update/:id" element={
+              <ProtectedRoute>
+              <UpdateAdPage />
+              </ProtectedRoute>
+              } />
             <Route path='/browse-ads' element={<SearchAds />} />
             <Route path='/browse-ads/:id' element={<SingleAdPage/>}/>
             <Route path='/profile' element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
                 <Profile />
-              // {/* </ProtectedRoute> */}
+              // </ProtectedRoute>
             } />
             
           </Routes>
